22e753d7e58bb9b924deb0e740115885be70a33d
ddavmy <ddavmy@gmail.com>
2023-03-28 08:01:56 +0200

    Dodany header, zmiany polozenia elementow, przycisk reset

--- php_01_kalkulator/app/calc_view.php
+++ php_01_kalkulator/app/calc_view.php
@@ -2,19 +2,23 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Kalkulator delty</title>
+	<title>Kalkulator funkcji kwadratowej</title>
 	<link rel="stylesheet" href="app/style.css">
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <div id="container">
+<h1 id="header">Postać ogólna<hr></h1>
+<div id="menu">
 <form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
     <input type="text" name="a" placeholder="Podaj a" autocomplete="off" value="<?php if(isset($a)) {print($a);}?>" />
     <input type="text" name="b" placeholder="Podaj b" autocomplete="off" value="<?php if(isset($b)) {print($b);}?>" />
     <input type="text" name="c" placeholder="Podaj c" autocomplete="off" value="<?php if(isset($c)) {print($c);}?>" />
-    <input id="button" value="Oblicz" type="submit"/>
+    <input class="button" type="submit" value="Oblicz"/>
+    <a href="<?php print(_APP_URL);?>"> <input class="button" type="button" value="Reset"/></a>
 </form>
+</div>
 
 <?php
 	if (isset($messages)) {
@@ -31,5 +35,6 @@
 <?php if (isset($result)) { 
 	echo '<div id="msg">Δ = '.$math.$result.'</div>';} ?>
 </div>
+
 </body>
 </html>
